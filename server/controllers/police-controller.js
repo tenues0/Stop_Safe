c63230707afc3927a09de0d188458766d3f2dd90
Theodore Ayoub <theodore.ayoub@gmail.com>
2022-08-31 19:03:43 -0700

    coded police routes

--- server/controllers/police-controller.js
+++ server/controllers/police-controller.js
@@ -1,4 +1,6 @@
 const { Police } = require('../models');
+// import user model
+const { User } = require('../models');
 // import sign token function from auth
 const { signToken } = require('../utils/auth');
 
@@ -41,4 +43,34 @@ module.exports = {
     const token = signToken(police);
     res.json({ token, police });
   },
+
+    // save ticket info to a user's `ticketsInfo` field by adding it to the set (to prevent duplicates)
+  // user comes from `req.user` created in the auth middleware function
+  async saveTicket({ user, body }, res) {
+    console.log(user);
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $addToSet: { ticketsInfo: body } },
+        { new: true, runValidators: true }
+      );
+      return res.json(updatedUser);
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json(err);
+    }
+  },
+  // remove a ticket from `ticketsInfo`
+  async deleteTicket({ user, params }, res) {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      { $pull: { ticketsInfo: { ticketId: params.ticketId } } },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "Couldn't find user with this id!" });
+    }
+    return res.json(updatedUser);
+  },
+
 };
