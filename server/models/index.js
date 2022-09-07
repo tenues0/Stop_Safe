011f93d8bada5484f64a764223f4cc90401e34a8
JinsaSameer <jinsajaleel@gmail.com>
2022-09-06 18:24:02 -0700

    structure changes

--- server/models/index.js
+++
@@ -1,8 +0,0 @@
-const Driver = require('./Driver');
-const Registration = require('./Registration');
-const Insurance = require('./Insurance');
-const Police = require('./Police');
-const Tickets = require('./Tickets');
-
-
-module.exports = { Driver, Registration, Insurance, Police, Tickets };
