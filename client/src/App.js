86c07864a08889a076acf513633ea699c0c1b479
Teddy <theodore.ayoub@gmail.com>
2022-09-09 14:55:22 -0700

    troubleshooting

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Routes>
         <Route 
           path='/' 
-          element={<SignupForm />} 
+          element={<DriverProfile />} 
         />
         <Route 
           path='/DriverProfile' 
