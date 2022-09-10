a3a292776b3c26311a4eccc57b09988ffb58f042
Teddy <theodore.ayoub@gmail.com>
2022-09-09 19:04:03 -0700

    front end up again

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header"
-import Footer from "./components/Footer"
-import LeftNavbar from "./components/LeftNavbar"
-import Account from "./components/Account" 
-import Contact from "./components/Contact"
-import Update from "./components/Update"
-import SignupForm from "./components/SignupForm"
+// import Footer from "./components/Footer"
+// import LeftNavbar from "./components/LeftNavbar"
+// import Account from "./components/Account" 
+// import Contact from "./components/Contact"
+// import Update from "./components/Update"
+// import SignupForm from "./components/SignupForm"
 import DriverProfile from './DriverProfile';
-import "./index.css";
+// import './App.css';
 
 function App() {
   return (
-    <Router>
+    // <Router>
     <>
-      <Header />
-      <Routes>
+    {/* <Router> */}
+      <DriverProfile />
+      {/* <Routes>
         <Route 
           path='/' 
           element={<DriverProfile />} 
@@ -28,9 +29,10 @@ function App() {
           path='*'
           element={<h1 className='display-2'>Wrong page!</h1>}
         />
-      </Routes>
+      </Routes> */}
+      {/* </Router> */}
     </>
-  </Router>
+  
   );
 }
 
